13ab1a3a8c7c6102550b0bf9eef89f8b34aba6d3
Iustin Negrea <iustin1010@gmail.com>
2026-06-22 05:42:14 +0300

    Refinements

--- api.php
+++ api.php
@@ -7,13 +7,14 @@ header("Content-Type: application/json; charset=UTF-8"); //Browserul stie ca e v
 // 1. Conectare la SQLite (creează fișierul automat dacă nu există)
 $db = new PDO('sqlite:database.db'); //( $ = o variabila, dar php stie automat;PDO este Librarie pt baze de date)
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+//$db->exec("DROP TABLE IF EXISTS reports");
 // 2. Creare tabelă dacă nu există (Prepared statements nativ pentru siguranță)
 $db->exec("CREATE TABLE IF NOT EXISTS reports (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     oras TEXT,
     cartier TEXT,
     categorie TEXT,
+    zona_depozitare TEXT,
     status TEXT DEFAULT 'Nerezolvat',
     data_creare DATE DEFAULT CURRENT_DATE
 )");
@@ -83,7 +84,7 @@ if ($action === 'export_pdf') {
     require('fpdf.php');
 
     $pdf = new FPDF();
-    $pdf->AdPage();
+    $pdf->AddPage();
     $pdf->SetFont('Arial','B', 16);
     //Titlu
     $pdf->Cell(0,10, 'Raport GaMon - Situatie', 0, 1,'C');
@@ -101,12 +102,14 @@ if ($action === 'export_pdf') {
     $pdf->SetFont('Arial','',12);
     $stmt = $db->query("SELECT * FROM reports");
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        $pdf->Cell(50,10,$cartier,1);
-        $pdf->Cell(60,10,$categorie,1);
-        $pdf->Cell(40,10, $row['status'],1);
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $pdf->Cell(15,10, $row['id'], 1);
+        $pdf->Cell(45,10, $row['cartier'], 1);
+        $pdf->Cell(50,10, $row['zona_depozitare'], 1);
+        $pdf->Cell(45,10, $row['categorie'], 1);
+        $pdf->Cell(35,10, $row['status'], 1);
         $pdf->Ln();
     }
-    $pdf->Output('D','Raport_GaMon.PDF');
+    $pdf->Output('D','Raport_GaMon.pdf');
     exit;
 }
